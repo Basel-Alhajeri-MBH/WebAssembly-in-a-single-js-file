ed2f45ee0bcd9d54422a6404463a5e0a255edf1b
Basel Al_hajeri.MBH?.() <56924872+Basel-Alhajeri-MBH@users.noreply.github.com>
2021-03-20 10:05:50 +0300

    update from async func to a default func to insert the Math.iadd
    Math.iadd either by WebAssembly or by asm.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-void async function WA() {
+void function WA() {
     //'(C) Basel.Al_hajeri?.MBH?.()'
     'use strict';
     let wat = 
-    String.raw`;;(C (MBH))
+        String.raw`;;(C (MBH))
 (module
   (func $iadd (export "iadd") (param i32 i32) (result i32)
     local.get 0
@@ -12,42 +12,47 @@ void async function WA() {
     let wasm_data = [
         0x0, 0x61, 0x73, 0x6d, 0x1, 0x0, 0x0, 0x0, 0x1, 0x7, 0x1, 0x60, 0x2, 0x7f, 0x7f, 0x1, 0x7f, 0x3, 0x2, 0x1, 0x0, 0x7, 0x8, 0x1, 0x4, 0x69, 0x61, 0x64, 0x64, 0x0, 0x0, 0xa, 0x9, 0x1, 0x7, 0x0, 0x20, 0x0, 0x20, 0x1, 0x6a, 0xb, 0x0, 0x17, 0x4, 0x6e, 0x61, 0x6d, 0x65, 0x1, 0x7, 0x1, 0x0, 0x4, 0x69, 0x61, 0x64, 0x64, 0x2, 0x7, 0x1, 0x0, 0x2, 0x0, 0x0, 0x1, 0x0
     ];
-    let iadd;
-    await new Promise(
-        resolve => resolve(
-            wasm_data
-        )
-    ).then(
-        byte => {
-            let buffer = new ArrayBuffer(byte.length);
-            let uint8 = new Uint8Array(buffer);
-            for (let i = 0; i !== byte.length; i++) {
-                uint8[ i ] = byte[ i ];
-            }
-            return buffer
+    let iadd, modules, $exports;
+    if (typeof WebAssembly==='object') {
+        let buffer = new ArrayBuffer(wasm_data.length);
+        let uint8 = new Uint8Array(buffer);
+        for (let i = 0; i !== wasm_data.length; i++) {
+            uint8[ i ] = wasm_data[ i ];
         }
-    ).then(
-        buffer => WebAssembly.compile(
+        modules = new WebAssembly.Module(
             buffer
-        )
-    ).then(
-        modules => new WebAssembly.Instance(
+        );
+        $exports = new WebAssembly.Instance(
             modules,
             {}
-        ).exports
-    ).then(
-        exports =>
-            iadd = exports.iadd
-    ).catch(
-        ()=>{
-            iadd = function iadd(i32a, i32b) {
+        ).exports;
+        iadd = $exports.iadd;
+    }
+        else {
+            iadd = function iadd(i32$a, i32$b) {
                 'use asm';
-                i32a = i32a | 0;
-                i32b = i32b | 0;
-                return (i32a + i32b) | 0;
+                i32$a = i32$a | 0;
+                i32$b = i32$b | 0;
+                return (i32$a + i32$b) | 0;
             };
         }
-    );
-    Math.iadd = iadd;// will do it later [BUSY]
+    Math.iadd = iadd;
+    
+    if(typeof module!=='undefined' && module.exports) {
+        module.exports = Math;
+    }
+    
+    // =======================
+    if (false)
+    for(let tests = [
+        [1, 1],
+        [8, 10],
+        [256, 256],
+        [0xFFFFFFFF, 0xFFFFFFFF]
+    ], i = 0, quiz = tests[ i ], leng = tests.length; i !== leng; quiz = tests[ i++ ], console.log(
+        `Math.iadd(${quiz}): ${Math.iadd(
+            ...quiz
+        )}`
+    ));
 }();
 
